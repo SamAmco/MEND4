9b28efebe97808989f6a67d058142b9757d7a716
Samuel Harvey-Collins <samuel.harvey-collins@conjure.co.uk>
2023-07-30 11:55:18 +0100

    Fixup locking script and readme to avoid dup runs

--- desktop-mend/example-scripts/Unix/mend-locker.sh
+++ desktop-mend/example-scripts/Unix/mend-locker.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+dupe_script=$(ps -ef | grep "mend-locker.sh" | grep -v grep | wc -l | xargs)
+
+if [ ${dupe_script} -gt 2 ]; then
+    echo -e "The SCRIPT_NAME.sh script was already running!"
+    exit 0
+fi
+
 dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" |
   while read x; do
     case "$x" in 
